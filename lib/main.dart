1077774c5a2f69337bda582451a88f6ce314baf2
Munikiran Narpareddi <narmuni123@gmail.com>
2022-06-17 13:32:20 +0530

    renaming

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/screens/leading_screen.dart';
+import 'package:weather_app/screens/loading_screen.dart';
 
 void main() {
   runApp(const MyApp());
